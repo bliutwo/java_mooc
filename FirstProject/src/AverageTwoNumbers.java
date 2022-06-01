8d37892c2af95f1348904b02a6450bc13155f500
bliutwo <bliutwo@cs.stanford.edu>
2022-06-01 00:10:31 -0700

    Finish 1.5

---
+++ FirstProject/src/AverageTwoNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AverageTwoNumbers {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Give the first number:");
+		int first = Integer.valueOf(scanner.nextLine());
+		System.out.println("Give the second number:");
+		int second = Integer.valueOf(scanner.nextLine());
+		double average = (first + second) / 2.0;
+		System.out.println("The average is " + average);
+		scanner.close();
+	}
+}
